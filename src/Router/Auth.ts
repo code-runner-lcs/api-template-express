dbbee6f362c3c7d390bee0bae58fe65180051587
Lucas lvn <lucas.lvn.dev@protonmail.com>
2026-01-25 20:49:19 +0100

    Add 'me' route in AuthRouter to retrieve current user information

--- src/Router/Auth.ts
+++ src/Router/Auth.ts
@@ -5,5 +5,6 @@ export const AuthRouter = (): Router => {
     const router = Router();
     router.post('/login', AuthController.login);
     router.post('/register', AuthController.register);
+    router.get('/me', AuthController.me);
     return router;
 }
