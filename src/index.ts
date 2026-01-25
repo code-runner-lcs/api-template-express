7ecb02639cdcace30bb86a357432227ed4d77c87
Lucas lvn <lucas.lvn.dev@protonmail.com>
2026-01-25 20:30:31 +0100

    Refactor index.ts to streamline imports and simplify server startup logging

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { AppDataSource } from './data-source';
+import Router from './router/Router';
 
 import "reflect-metadata";
-import Router from './router/Router';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,14 +11,11 @@ app.use(express.json());
 
 AppDataSource.initialize()
     .then(async () => {
-        console.log('BDD Connexion OK');
-
         const router = new Router(app);
+
         await router.init();
 
-        app.listen(PORT, () => {
-            console.log(`Server running on http://localhost:${PORT}`);
-        });
+        app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
     })
     .catch((error) => {
         console.error('Error connecting to the database:', error);
