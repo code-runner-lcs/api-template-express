dc4d6a2ef436db61e8be61ee043f5f9a2b42f71f
Lucas lvn <lucas.lvn.dev@protonmail.com>
2026-01-27 14:17:23 +0100

    Update public routes in middleware to rename password reset route and add new reset-password route, enhancing clarity and functionality.

--- src/middeware/public.ts
+++ src/middeware/public.ts
@@ -1,6 +1,13 @@
+/**
+ * Public routes
+ * @type {Array}
+ * @property {string} path - The path of the route
+ * @property {string} method - The method of the route
+ */
 export const publicRoutes = [
     { path: '/auth/login', method: 'POST' },
     { path: '/auth/register', method: 'POST' },
-    { path: '/auth/password-reset', method: 'POST' },
+    { path: '/auth/ask-password-reset', method: 'POST' },
+    { path: '/auth/reset-password', method: 'POST' },
     { path: '/auth/confirm-email', method: 'GET' },
 ];
